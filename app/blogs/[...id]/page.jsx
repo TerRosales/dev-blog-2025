6715c9ad6b5d65700ba17eda5e2ca59faa10b5cf
TerRosales <rtin1027411@gmail.com>
2025-04-28 11:31:48 -0400

    Navbar

---
+++ app/blogs/[...id]/page.jsx
@@ -0,0 +1,5 @@
+const page = ({ params }) => {
+  return <div>Blogs Page {params.id}</div>;
+};
+
+export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>Har</div>;
+  return (
+    <div>
+      <p>
+        <Link href="/blogs">Go to Blogs</Link>
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
